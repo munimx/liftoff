73357a7aedb2774326c918fafee1c67987050f49
Munim-Ahmad <munimahmad2@gmail.com>
2026-05-02 19:41:46 +0500

    feat(monitoring): add imports for ProjectsModule and DoApiModule

--- apps/api/src/monitoring/monitoring.module.ts
+++ apps/api/src/monitoring/monitoring.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MonitoringController } from './monitoring.controller';
 import { MonitoringService } from './monitoring.service';
+import { ProjectsModule } from '../projects/projects.module';
+import { DoApiModule } from '../do-api/do-api.module';
 
 /**
  * Monitoring module for logs and metrics.
  */
 @Module({
+  imports: [ProjectsModule, DoApiModule],
   controllers: [MonitoringController],
   providers: [MonitoringService],
   exports: [MonitoringService],
